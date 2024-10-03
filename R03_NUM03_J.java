e28c48ed25400eabe608910e54dc03a304d4ed9b
Kenneth Lanza-Pego <klanza03@outlook.com>
2024-10-03 12:07:52 -0400

    Updated R03_NUM03_J.java file to contain compliant code as per rule 03

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,6 +1,6 @@
 public class R03_NUM03_J {
     
     public static int getInteger(DataInputStream is) throws IOException{
-        return is.readInt();
+        return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
     }
 }
