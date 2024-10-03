cc5567b29b23ad2ebcf0baec0f43545d7c7d603d
Kenneth Lanza-Pego <klanza03@outlook.com>
2024-10-03 11:54:30 -0400

    Added R02_XP00_J.java file to repo containing non-compliant code as per rule 02.

---
+++ R02_XP00_J.java
@@ -0,0 +1,10 @@
+public class R02_XP00_J {
+    
+    public void deleteFile(){
+
+        File someFile = new File("someFileName.txt");
+        // do something with said file
+        someFile.delete();
+    }
+
+}
