529dd1eec6c01e26107743b3be0fc22558326b44
Kenneth Lanza-Pego <klanza03@outlook.com>
2024-10-06 11:55:34 -0400

    Updated R14_SER06_J.java code to be compliant with Rule 14. Serialization

--- R14_SER06_J.java
+++ R14_SER06_J.java
@@ -2,17 +2,11 @@
 // Rule 14. Serialization (SER)
 // Example: SER06-J: Make defensive copies of private mutable components during deserialization
 
-class MutableSer implements Serializable {
-    private static final Date epoch = new Date(0);
-    private Date date = null; // Mutable component
-     
-    public MutableSer(Date d){
-      date = new Date(d.getTime()); // Constructor performs defensive copying
-    }
-   
-    private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
-      ois.defaultReadObject();
-      // Perform validation if necessary
-    }
+private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+    ObjectInputStream.GetField fields = ois.readFields();
+    Date inDate = (Date) fields.get("date", epoch);
+    // Defensively copy the mutable component
+    date = new Date(inDate.getTime());
+    // Perform validation if necessary
   }
 
