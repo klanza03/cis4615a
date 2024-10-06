4de73f46e1b03bafb0dcba9319e5ab52d86dee8a
Kenneth Lanza-Pego <klanza03@outlook.com>
2024-10-06 12:29:50 -0400

    Added R08_VNA00_J.java file with non-compliant code as per Rule 08. Visibility and Atomicity

---
+++ R08_VNA00_J.java
@@ -0,0 +1,22 @@
+
+// Rule 08. Visibility and Atomicity
+// Example VNA00-J: Ensure visibility when accessing shared primitive variables
+
+final class ControlledStop implements Runnable {
+    private boolean done = false;
+    
+    @Override public void run() {
+      while (!done) {
+        try {
+          // ...
+          Thread.currentThread().sleep(1000); // Do something
+        } catch(InterruptedException ie) {
+          Thread.currentThread().interrupt(); // Reset interrupted status
+        }
+      }   
+    }
+   
+    public void shutdown() {
+      done = true;
+    }
+  }
