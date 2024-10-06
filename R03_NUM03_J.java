19939988c004917397bbcc05e0c7c112db1f7aeb
Kenneth Lanza-Pego <klanza03@outlook.com>
2024-10-06 13:04:32 -0400

    Added comments to R03_NUM03_J.java explaining rule number and examle presented

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,3 +1,7 @@
+
+// Rule 03. Numeric Types and Operations
+// Example NUM03-J: Use integer types that can fully represent the possible range of unsigned data
+
 public class R03_NUM03_J {
     
     public static int getInteger(DataInputStream is) throws IOException{
