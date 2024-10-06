e24755a2c331ff1780c51aa22c6d13d551ec9524
Kenneth Lanza-Pego <klanza03@outlook.com>
2024-10-06 10:41:05 -0400

    Updated code to be compliant with Rule 04. Characters and Strings

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,10 +1,9 @@
-import java.math.BigInteger;
 
 public class R04_STR03_J {
     
     BigInteger x = new BigInteger("530500452766");
-    byte[] byteArray = x.toByteArray();
-    String s = new String(byteArray);
-    byteArray = s.getBytes();
-    x = new BigInteger(byteArray);
+    String s = x.toString(); // Valid Character data
+    byte[] byteArray = s.getBytes();
+    String ns = new String(byteArray);
+    x = new BigInteger(ns);
 }
